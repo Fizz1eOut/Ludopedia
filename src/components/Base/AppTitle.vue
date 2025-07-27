b2022dbd532b0f7ef26e356c4e1c914ea79637d6
FizzleOut <fizzleout1337@gmail.com>
2025-07-27 13:39:55 +0200

    feat(ui): added AppTitle component
    - h2 element with Inter font and bold style
    - White text color
    - Accepts content via slot

---
+++ src/components/Base/AppTitle.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  
+</script>
+<template>
+  <h2 class="title">
+    <slot></slot>
+  </h2>
+</template>
+<style scoped>
+  .title {
+    font-family: "Inter", sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    color: var(--color-white);  
+    }  
+</style>
