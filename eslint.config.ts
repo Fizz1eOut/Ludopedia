5a22bc24aa8b2c4e226ec3991abd144a02826779
FizzleOut <fizzleout1337@gmail.com>
2025-07-27 13:10:50 +0200

    chore(eslint): add and configure ESLint for Vue + TypeScript project
    - Configures Vue-specific rules (vue/max-attributes-per-line, vue/script-indent, etc.)
    - Adds base formatting rules (quotes, semi, indent, etc.)
    - Ignores dist, coverage and other output directories
    - Includes plugins: vue, @vue/eslint-config-typescript, vitest

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,26 +1,53 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVitest from '@vitest/eslint-plugin';
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
-
-export default defineConfigWithVueTs(
+export default [
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+    rules: {
+      'vue/max-attributes-per-line': ['warn', {
+        singleline: 3,
+        multiline: 3
+      }],
+      'vue/singleline-html-element-content-newline': ['off'],
+      'eol-last': ['error', 'always'],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2, {
+        SwitchCase: 1,
+        ignoredNodes: [
+          'TemplateLiteral',
+          'Program > ExpressionStatement > TemplateLiteral',
+          'Program',
+          'ExpressionStatement > CallExpression'
+        ]
+      }],
+      'vue/script-indent': ['error', 2, {
+        baseIndent: 1,
+        switchCase: 1,
+        ignores: []
+      }],
+      'vue/html-indent': ['error', 2, {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true
+      }]
+    }
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
 
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-  
+  ...pluginVue.configs['flat/essential'],
+  ...defineConfigWithVueTs(vueTsConfigs.recommended),
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-)
+];
