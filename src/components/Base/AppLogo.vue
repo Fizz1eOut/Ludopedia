133b0fb3579246aa7b93129506f3391a72d23fce
FizzleOut <fizzleout1337@gmail.com>
2025-07-28 18:40:49 +0200

    feat(ui): added AppLogo component
    - Displays the logo with an icon and "Ludopedia" name
    - Uses gradient background and text styling
    - Theme-aware via CSS variables

---
+++ src/components/Base/AppLogo.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+  import AppIcon from '@/components/Base/AppIcon.vue';
+</script>
+
+<template>
+  <div class="logo">
+    <div class="logo__icon">
+      <app-icon 
+        name="logo" 
+        size="var(--space-lg)"
+        color="var(--blue-50)"
+        class="icon"
+      />
+    </div>
+    <div class="logo__name">Ludopedia</div>
+  </div>
+</template>
+
+<style scoped>
+  .logo {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+  .logo__icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 50px;
+    padding: 6px 10px;
+    background: var(--gradient-logo);
+    border-radius: var(--radius-md);
+  }
+  .icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .logo__name {
+    font-size: var(--font-size-3xl);
+    background: var(--gradient-logo);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    color: transparent;
+  }
+</style>
