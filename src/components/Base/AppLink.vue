caccd49d2d5e0a3c0df2c104900aeca9cccb7229
FizzleOut <fizzleout1337@gmail.com>
2025-07-28 18:00:48 +0200

    feat(ui): added AppLink component
    - Displays a link with blue accent styling
    - Hover effect with background darkening
    - Uses theme variables and Inter font

---
+++ src/components/Base/AppLink.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <div class="link line">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.link {
+  font-family: "Inter", sans-serif;
+  font-size: var(--font-size-md);
+  font-weight: var(--font-weight-regular);
+  color: var(--blue-500);
+  transition: background-color 0.3s ease-in-out;
+  cursor: pointer;
+}
+.link:hover {
+  background-color: var(--blue-600);
+}
+</style>
