30b1812624ac87d4c88db6ab0464820eb3c589fb
FizzleOut <fizzleout1337@gmail.com>
2025-08-05 19:10:37 +0200

    build(vite): update Vite config
    - Set base path to /Ludopedia
    - Added svgLoader for importing SVGs as Vue components
    - Added test section for Vitest:
      - Enabled globals and jsdom environment
      - Added code coverage with Istanbul
    - Configured proxy for /igdb-api requests (via https://ludopediaproxy.onrender.com)

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,42 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import svgLoader from 'vite-svg-loader';
+import { configDefaults } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Ludopedia',
   plugins: [
     vue(),
     vueDevTools(),
+    svgLoader(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/tests/**/*.test.{ts,js}'],
+    exclude: [...configDefaults.exclude, 'e2e/**'],
+    coverage: {
+      provider: 'istanbul',
+      reportsDirectory: 'coverage',
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+  server: {
+    proxy: {
+      '/igdb-api': {
+        target: 'https://ludopediaproxy.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/igdb-api/, ''),
+      },
+    },
+  }
+
+});
