6f4e827ddf2b84980994d8944b29c17b93de66b4
FizzleOut <fizzleout1337@gmail.com>
2025-08-08 07:46:17 +0200

    feat(components): add basic AppFavorites component
    - Displays a "favorite" icon
    - Currently contains only minimal markup and styles
    - Will be expanded and enhanced in future updates

---
+++ src/components/Base/AppFavorites.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import AppIcon from '@/components/Base/AppIcon.vue';
+</script>
+
+<template>
+  <div class="favorites">
+    <app-icon
+      name="favorite"
+      size="30px"
+      color="var(--text-primary)"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
