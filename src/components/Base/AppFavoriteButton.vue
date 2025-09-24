0fec442bb8110d14f44a2ca43819f621f403814e
FizzleOut <fizzleout1337@gmail.com>
2025-09-24 18:55:16 +0200

    feat(AppFavoriteButton): add favorite button component for games
    - "Add to favourites" button with icon
    - Authenticated users can toggle favorites directly
    - Unauthenticated users see modal with AuthForm
    - Supports re-adding after successful login

---
+++ src/components/Base/AppFavoriteButton.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+  import { ref }  from 'vue';
+  import AppButton from '@/components/Base/AppButton.vue';
+  import AppIcon from '@/components/Base/AppIcon.vue';
+  import AppModal from '@/components/Base/AppModal.vue';
+  import AuthForm from '@/components/Content/Auth/AuthForm.vue';
+  import { useFavoritesStore } from '@/stores/favoritesStore';
+  import { useAuthStore } from '@/stores/authStore';
+  import type { GameResponse } from '@/interface/game.interface';
+
+  interface AppFavoriteButtonProps {
+    game: GameResponse;
+  }
+  defineProps<AppFavoriteButtonProps>();
+
+  const authStore = useAuthStore();
+  const favoritesStore = useFavoritesStore();
+  const isModalOpen = ref(false);
+
+  const toggleFav = async (itemId: string) => {
+    if (authStore.isAuthenticated) {
+      await favoritesStore.toggleFavorite(itemId);
+    } else {
+      isModalOpen.value = true;
+    }
+  };
+
+  const onAuthSuccess = async (itemId: string) => {
+    isModalOpen.value = false;
+    await favoritesStore.toggleFavorite(itemId);
+  };
+
+</script>
+
+<template>
+  <div class="favorites-button">
+    <app-button 
+      secondary 
+      class="favorites-button__btn"
+      @click="toggleFav(game.id.toString())"
+    >
+      <app-icon 
+        :name="favoritesStore.isFavorite(game.id.toString()) ? 'heartfull' : 'heart'"
+        size="20px"
+        color="var(--white)"
+      />
+      {{ favoritesStore.isFavorite(game.id.toString()) ? 'Added' : 'Add to favourites' }}
+    </app-button>
+
+    <app-modal
+      :model-value="isModalOpen"
+      @update:model-value="isModalOpen = $event"
+    >
+      <auth-form @close="isModalOpen = false"  @authSuccess="onAuthSuccess(game.id.toString())"  />
+    </app-modal>
+  </div>
+</template>
+
+<style scoped>
+  .favorites-button__btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+  }
+</style>
