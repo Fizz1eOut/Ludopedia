26fc2c2e633410da8003697b9229e90ec8effcb2
FizzleOut <fizzleout1337@gmail.com>
2025-09-29 18:50:46 +0200

    feat(AppGameCard): add game card component
    - New AppGameCard component to display game information
    - Shows cover image, name, release date, platforms, and genres
    - Added tags for rating and genres
    - Integrated AppFavoriteButton in "icon" mode for adding to favorites
    - Added "View Product" button to navigate to game details

--- src/components/Base/AppGameCard.vue
+++ src/components/Base/AppGameCard.vue
@@ -15,48 +15,51 @@
 </script>
 
 <template>
-  <div class="wrapper game-card__wrapper">
-    <app-underlay>
-      <app-container>
+  <div class="wrapper">
+    <app-underlay class="underlay">
+      <app-container class="container">
         <div class="game-card">
-          <div class="game-card__image">
-            <app-image 
-              :url="game.cover?.url" 
-              size="t_cover_big" 
-              class="game-card__img"
-            />
-            <app-tag
-              type="chip"
-              :rating="game.rating"
-              class="game-card__rating"
-            />
-            <app-favorite-button type="icon" :game="game" class="game-card__favorite" />
-          </div>
-          <div class="game-card__content">
-            <div class="game-card__name">{{ game.name }}</div>
-            <div v-if="game.first_release_date" class="game-card__released">
-              <app-icon 
-                name="date"
-                size="14px"
-                color="var(--text-secondary)"
+          <div class="game-card__group">
+            <div class="game-card__image">
+              <app-image 
+                :url="game.cover?.url" 
+                size="t_cover_big" 
+                class="game-card__img"
               />
-              {{ new Date(game.first_release_date * 1000).toLocaleDateString() }}
+              <app-tag
+                v-if="game.rating"
+                type="chip"
+                :rating="game.rating"
+                class="game-card__rating"
+              />
+              <app-favorite-button type="icon" :game="game" class="game-card__favorite" />
             </div>
-            <div class="game-card__platform">
-              <app-tag 
-                v-for="platform in game.platforms"
-                :key="platform.id"
-                type="badge"
-                :text="platform.name"
-              />
-            </div>
-            <div class="game-card__genres">
-              <app-tag 
-                v-for="genre in game.genres"
-                :key="genre.id"
-                type="success"
-                :text="genre.name"
-              />
+            <div class="game-card__content">
+              <div class="game-card__name">{{ game.name }}</div>
+              <div v-if="game.first_release_date" class="game-card__released">
+                <app-icon 
+                  name="date"
+                  size="14px"
+                  color="var(--text-secondary)"
+                />
+                {{ new Date(game.first_release_date * 1000).toLocaleDateString() }}
+              </div>
+              <div class="game-card__platform">
+                <app-tag 
+                  v-for="platform in game.platforms"
+                  :key="platform.id"
+                  type="badge"
+                  :text="platform.name"
+                />
+              </div>
+              <div class="game-card__genres">
+                <app-tag 
+                  v-for="genre in game.genres"
+                  :key="genre.id"
+                  type="success"
+                  :text="genre.name"
+                />
+              </div>
             </div>
           </div>
           <div class="game-card__button">
@@ -69,21 +72,27 @@
 </template>
 
 <style scoped>
-  .wrapper {
-    max-width: 300px;
+  .wrapper,
+  .underlay {
+    display: flex;
+    flex: 1 1 auto;
+    height: auto;
+  }
+  .container {
     width: 100%;
   }
   .game-card {
-    max-width: 300px;
-    width: 100%;
     display: flex;
-    justify-content: space-between;
     flex-direction: column;
+    justify-content: space-between;
     gap: 20px;
+    height: 100%;
   }
   .game-card__image {
     position: relative;
-    max-width: 300px;
+  }
+  .game-card__img {
+    height: 300px;
   }
   :deep(.app-tag--chip) {
     padding: 4px 8px;
@@ -108,9 +117,15 @@
     margin-bottom: 10px;
   }
   .game-card__name {
+    margin-top: var(--space-sm);
     font-size: var(--font-size-lg);
     font-weight: var(--font-weight-medium);
     color: var(--text-primary);
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    height: 42px;
   }
   .game-card__platform,
   .game-card__genres {
@@ -126,5 +141,13 @@
     font-size: var(--font-size-sm);
     font-weight: var(--font-weight-regular);
     color: var(--text-primary);
+  }
+  @media (max-width: 500px) {
+     .game-card__img {
+      height: 350px;
+    }
+    :deep(.image) {
+      object-fit: fill;
+    }
   }
 </style>
