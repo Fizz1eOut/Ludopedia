9821f3893b0542cb37193d52a94a1d9ab8d2e098
FizzleOut <fizzleout1337@gmail.com>
2025-10-05 14:12:41 +0200

    feat(FavoritesClearModal): add modal for clearing favorite games
    - Created FavoritesClearModal component
    - Added confirm and cancel buttons
    - Implemented `close` and `clear` emits
    - Added responsive styles for small screens

---
+++ src/components/Content/Favorites/FavoritesClearModal.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+  import AppTitle from '@/components/Base/AppTitle.vue';
+  import AppButton from '@/components/Base/AppButton.vue';
+  import AppIcon from '@/components/Base/AppIcon.vue';
+</script>
+
+<template>
+  <div class="favorites-clear-modal">
+    <div class="favorites-clear-modal__header">
+      <app-title>
+        Clear favorites
+      </app-title>
+      <app-button class="auth-form__close" @click="$emit('close')">
+        <app-icon
+          name="cross"
+          size="20px"
+          style="color: var(--white)"
+        />
+      </app-button>
+    </div>
+
+    <div class="favorites-clear-modal__content">
+      <p class="favorites-clear-modal__text">
+        Are you sure you want to clear your entire list of favorite games?
+      </p>
+      <div class="favorites-clear-modal__actions">
+        <app-button primary @click="$emit('close')">
+          No, keep my favorites
+        </app-button>
+        <app-button secondary @click="$emit('clear')">
+          Yes, clear favorites
+        </app-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .favorites-clear-modal__header {
+    margin-bottom: var(--space-md);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+  }
+  .auth-form__close {
+    width: 30px;
+    height: 30px;
+  }
+  .favorites-clear-modal__text {
+    font-size: var(--font-size-lg);
+    color: var(--text-primary);
+    margin-bottom: var(--space-md);
+  }
+  .favorites-clear-modal__actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+  }
+  @media (max-width: 380px) {
+    :deep(.button) {
+      font-size: var(--font-size-sm);
+    }
+  }
+</style>
