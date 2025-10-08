3b8938e7e25e4c0aa9ae2e8e81381cd68eb5a6a7
FizzleOut <fizzleout1337@gmail.com>
2025-10-08 20:58:50 +0200

    feat(HomeView): added NewReleases and GameCategories sections
    - Imported and integrated NewReleases and GameCategories components
    - Home page now includes popular games, new releases, and category sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
   import PopularGamesHero from '@/components/Content/Home/PopularGamesHero.vue';
+  import NewReleases from '@/components/Content/Home/NewReleases.vue';
+  import GameCategories from '@/components/Content/Home/GameCategories.vue';
 </script>
 
 <template>
   <popular-games-hero />
+  <new-releases />
+  <game-categories />
 </template>
 
 <style scoped>
